c4b1b5f2ea805b191456bc82e41a7bedabaa5e8e
bulkersguide-debug <bulkersguide@gmail.com>
2026-05-16 00:27:52 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -10,23 +10,21 @@ import PrivacyPolicy from "./PrivacyPolicy";
 import Contact from "./Contact";
 import RivalriesPage from "./RivalriesPage";
 import RequestFandomPage from "./RequestFandomPage";
-import BuyBannerPage from "./BuyBannerPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/"                element={<App />} />
-        <Route path="/fandoms"         element={<FandomDirectory />} />
-        <Route path="/fandom/:slug"    element={<FandomPage />} />
-        <Route path="/fandoms/:slug"   element={<FandomPage />} />
-        <Route path="/admin"           element={<AdminPage />} />
-        <Route path="/terms"           element={<TermsOfService />} />
-        <Route path="/privacy"         element={<PrivacyPolicy />} />
-        <Route path="/contact"         element={<Contact />} />
-        <Route path="/rivalries"       element={<RivalriesPage />} />
-        <Route path="/request-fandom"  element={<RequestFandomPage />} />
-        <Route path="/advertise"       element={<BuyBannerPage />} />
+        <Route path="/"               element={<App />} />
+        <Route path="/fandoms"        element={<FandomDirectory />} />
+        <Route path="/fandom/:slug"   element={<FandomPage />} />
+        <Route path="/fandoms/:slug"  element={<FandomPage />} />
+        <Route path="/admin"          element={<AdminPage />} />
+        <Route path="/terms"          element={<TermsOfService />} />
+        <Route path="/privacy"        element={<PrivacyPolicy />} />
+        <Route path="/contact"        element={<Contact />} />
+        <Route path="/rivalries"      element={<RivalriesPage />} />
+        <Route path="/request-fandom" element={<RequestFandomPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
